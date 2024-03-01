e2b08cbfebaa060b93874192eb5c3c1fb7138f29
Tom <tmaltby19@gmail.com>
2024-03-01 19:56:01 +0700

    init

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,62 +1,62 @@
-import { NextResponse } from 'next/server'
-import { match } from '@formatjs/intl-localematcher'
-import Negotiator from 'negotiator'
-import { Locale, isLocale, locales } from '@/utils/enums/locale'
-import type { NextRequest } from 'next/server'
+// import { NextResponse } from 'next/server'
+// import { match } from '@formatjs/intl-localematcher'
+// import Negotiator from 'negotiator'
+// import { Locale, isLocale, locales } from '@/utils/enums/locale'
+// import type { NextRequest } from 'next/server'
 
-// Helper function to convert Next.js Headers to a simple object
-function headersToObject(headers: Headers): { [key: string]: string } {
-    const headersObj: { [key: string]: string } = {}
-    headers.forEach((value, key) => {
-        headersObj[key] = value
-    })
-    return headersObj
-}
+// // Helper function to convert Next.js Headers to a simple object
+// function headersToObject(headers: Headers): { [key: string]: string } {
+//     const headersObj: { [key: string]: string } = {}
+//     headers.forEach((value, key) => {
+//         headersObj[key] = value
+//     })
+//     return headersObj
+// }
 
-// Function to get locale from cookie
-function getLocaleFromCookie(request: NextRequest): string | null {
-    const cookie = request.cookies.get('mshrmLocale')
+// // Function to get locale from cookie
+// function getLocaleFromCookie(request: NextRequest): string | null {
+//     const cookie = request.cookies.get('mshrmLocale')
 
-    return cookie ? cookie.value : null
-}
+//     return cookie ? cookie.value : null
+// }
 
-// Get the preferred locale, similar to the above or using a library
-function getLocaleFromHeaders(request: NextRequest) {
-    const headers = headersToObject(request.headers)
-    const languages = new Negotiator({ headers }).languages()
-    const defaultLocale = Locale.English
-    return match(languages, locales, defaultLocale)
-}
+// // Get the preferred locale, similar to the above or using a library
+// function getLocaleFromHeaders(request: NextRequest) {
+//     const headers = headersToObject(request.headers)
+//     const languages = new Negotiator({ headers }).languages()
+//     const defaultLocale = Locale.English
+//     return match(languages, locales, defaultLocale)
+// }
 
-export function middleware(request: NextRequest) {
-    // Check if there is any supported locale in the pathname
-    const { pathname } = request.nextUrl
-    const pathnameHasLocale = locales.some(
-        (locale) =>
-            pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-    )
+// export function middleware(request: NextRequest) {
+//     // Check if there is any supported locale in the pathname
+//     const { pathname } = request.nextUrl
+//     const pathnameHasLocale = locales.some(
+//         (locale) =>
+//             pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+//     )
 
-    if (pathnameHasLocale) return
+//     if (pathnameHasLocale) return
 
-    const localeFromCookie = getLocaleFromCookie(request)
+//     const localeFromCookie = getLocaleFromCookie(request)
 
-    const locale = isLocale(localeFromCookie)
-        ? localeFromCookie
-        : getLocaleFromHeaders(request)
+//     const locale = isLocale(localeFromCookie)
+//         ? localeFromCookie
+//         : getLocaleFromHeaders(request)
 
-    request.nextUrl.pathname = isLocale(locale)
-        ? `/${locale}${pathname}`
-        : `/en${pathname}`
-    // e.g. incoming request is /products
-    // The new URL is now /en/products
-    return NextResponse.redirect(request.nextUrl)
-}
+//     request.nextUrl.pathname = isLocale(locale)
+//         ? `/${locale}${pathname}`
+//         : `/en${pathname}`
+//     // e.g. incoming request is /products
+//     // The new URL is now /en/products
+//     return NextResponse.redirect(request.nextUrl)
+// }
 
-export const config = {
-    matcher: [
-        // Skip all internal paths (_next)
-        '/((?!_next).*)',
-        // Optional: only run on root (/) URL
-        // '/'
-    ],
-}
+// export const config = {
+//     matcher: [
+//         // Skip all internal paths (_next)
+//         '/((?!_next).*)',
+//         // Optional: only run on root (/) URL
+//         // '/'
+//     ],
+// }
