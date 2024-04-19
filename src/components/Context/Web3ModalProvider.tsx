6f0ffd75c73386b2964b1ddf8df6699e5de810c2
Tom <tmaltby19@gmail.com>
2024-04-19 13:21:05 +0700

    web3modal test

--- src/components/Context/Web3ModalProvider.tsx
+++ src/components/Context/Web3ModalProvider.tsx
@@ -1,12 +1,10 @@
 'use client'
 
-import Web3Modal from '@/utils/context/Web3Modal'
+import dynamic from 'next/dynamic'
 
-// import dynamic from 'next/dynamic'
-
-// const Web3Modal = dynamic(() => import('@/utils/context/Web3Modal'), {
-//     ssr: false,
-// })
+const Web3Modal = dynamic(() => import('@/utils/context/Web3Modal'), {
+    ssr: false,
+})
 
 export default function Web3ModalProvider({
     children,
