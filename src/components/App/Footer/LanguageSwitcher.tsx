954575effd201e2adea5aae83a5c192ad95bfeb4
Tom <tmaltby19@gmail.com>
2024-08-26 19:50:38 +0700

    misc ui updates

--- src/components/App/Footer/LanguageSwitcher.tsx
+++ src/components/App/Footer/LanguageSwitcher.tsx
@@ -7,22 +7,25 @@ export default function FooterLanguageSwitcher() {
     const { changeLanguage, options } = useLanguage()
 
     return (
-        <ul className={styles.ul}>
-            {options.map((option) => (
-                <li key={option.value}>
-                    <button
-                        className={
-                            option.active
-                                ? styles.activeLocale
-                                : styles.inactiveLocale
-                        }
-                        disabled={option.active}
-                        onClick={() => changeLanguage(option.value)}
-                    >
-                        {option.label}
-                    </button>
-                </li>
-            ))}
-        </ul>
+        <nav aria-label="TODO (translate): Language Switcher">
+            <ul className={styles.ul}>
+                {options.map((option) => (
+                    <li key={option.value}>
+                        <button
+                            className={
+                                option.active
+                                    ? styles.activeLocale
+                                    : styles.inactiveLocale
+                            }
+                            disabled={option.active}
+                            lang={option.value}
+                            onClick={() => changeLanguage(option.value)}
+                        >
+                            {option.label}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     )
 }
