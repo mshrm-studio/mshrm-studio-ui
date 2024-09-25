6392cf80a8fb888c822036e44d667f3e24e1fe91
Tom <tmaltby19@gmail.com>
2024-09-25 20:13:57 +0700

    add contact page

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,21 +1,28 @@
+import { locales } from '@/utils/enums/Locale'
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const publicUrl = process.env.NEXT_PUBLIC_URL || 'https://mshrm.studio'
+    const publicUrl = process.env.NEXT_PUBLIC_URL || 'https://www.mshrm.studio'
 
     return [
         {
             url: publicUrl,
             lastModified: new Date(),
             alternates: {
-                languages: {
-                    ar: `${publicUrl}/ar`,
-                    de: `${publicUrl}/de`,
-                    en: `${publicUrl}/en`,
-                    es: `${publicUrl}/es`,
-                    km: `${publicUrl}/km`,
-                    zh: `${publicUrl}/zh`,
-                },
+                languages: locales.reduce((acc, locale) => {
+                    acc[locale] = `${publicUrl}/${locale}`
+                    return acc
+                }, {} as Record<string, string>),
+            },
+        },
+        {
+            url: `${publicUrl}/contact`,
+            lastModified: new Date(),
+            alternates: {
+                languages: locales.reduce((acc, locale) => {
+                    acc[locale] = `${publicUrl}/${locale}/contact`
+                    return acc
+                }, {} as Record<string, string>),
             },
         },
     ]
